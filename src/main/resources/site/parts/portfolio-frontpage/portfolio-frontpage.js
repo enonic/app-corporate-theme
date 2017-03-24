65f8a6c0bdbd78afc742493706116b38a46f14ef
Sepideh <ska@enonic.com>
2017-03-24 15:14:09 +0100

    Portfolio frontpage

---
+++ src/main/resources/site/parts/portfolio-frontpage/portfolio-frontpage.js
@@ -0,0 +1,49 @@
+var libs = {
+    portal : require('/lib/xp/portal'),
+    thymeleaf : require('/lib/xp/thymeleaf'),
+    contentLib : require('/lib/xp/content'),
+    util : require('/lib/enonic/util')
+};
+
+var view = resolve('portfolio-frontpage.html');
+
+exports.get = function(req){
+
+    var config = libs.portal.getComponent().config;
+    var portfolios = [];
+    var portfolioArray = config.portfolio ? libs.util.data.forceArray(config.portfolio) : null;
+
+    if(portfolioArray){
+        for(var i = 0; i < portfolioArray.length; i++ ){
+            var portfolioKey = libs.contentLib.get({
+                key : portfolioArray[i]
+            });
+
+            if(portfolioKey){
+                var image = libs.portal.imageUrl({
+                    id : portfolioKey.data.portfolioImage,
+                    scale: 'block(1024,768)',
+                });
+
+                var portfolioObject = {
+                    portfolioImage : image,
+                    portfolioTitle : portfolioKey.data.portfolioTitle,
+                    portfolioIntro : portfolioKey.data.portfolioIntro
+
+                };
+
+                if(portfolioObject){
+                    portfolios.push(portfolioObject );
+                }
+            }
+        }
+    }
+    var model = {
+        title : config.partTitle,
+        intro : config.partIntro,
+        portfolios : portfolios
+    };
+    var body = libs.thymeleaf.render(view, model);
+    return {body : body};
+
+};
