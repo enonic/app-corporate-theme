48550641aea868efe578f77510c79171d1785cb1
Sepideh <ska@enonic.com>
2017-03-24 15:15:28 +0100

    Contact post method

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -1,13 +1,25 @@
 jQuery(function($) {
 
 	//Ajax contact
-	var form = $('.contact-form');
-		form.submit(function () {
-			$this = $(this);
-			$.post($(this).attr('action'), function(data) {
-			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
-		return false;
+	$('.contact-form').submit(function() {
+		$('button').click(function(event) {
+			event.preventDefault();
+		});
+		$this = $(this);
+
+//create a json with dynamic variable
+		var formData = {};
+		$('.contact-form .post-data').each(function(){
+			formData[$(this).attr('name')]= $(this).val();
+		});
+
+		$.post($(this).attr('action'), formData, function(data) {
+				$('.formFields').parent().append(data);
+				$('.formFields').hide();
+				$('.formFields').parent().before($('.response-field'));
+
+			},'html');
+	return false;
 	});
 
 	//Goto Top
@@ -17,6 +29,5 @@ jQuery(function($) {
 			 scrollTop: $("body").offset().top
 		 }, 500);
 	});	
-	//End goto top		
-
+	//End goto top
 });
