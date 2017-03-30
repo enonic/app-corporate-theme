2aaef2360ca6b8b388c921fcf8d33c80a85f6604
Bobby <bellfalasch@gmail.com>
2017-03-30 13:30:47 +0200

    Remove use of ul/li in view file

--- src/main/resources/site/parts/service-frontpage/service-frontpage.js
+++ src/main/resources/site/parts/service-frontpage/service-frontpage.js
@@ -10,7 +10,7 @@ var view = resolve('service-frontpage.html');
 exports.get = function(req){
 
     var config = libs.portal.getComponent().config;
-var services = [];
+	 var services = [];
     var serviceArray = config.service ? libs.util.data.forceArray(config.service) : null;
 
     if(serviceArray){
@@ -19,22 +19,23 @@ var services = [];
                 key : serviceArray[i]
             });
 
-            var serviceObject = {
-                icon : serviceKey.data.iconFrontPage,
-                serviceTitle : serviceKey.data.serviecTitle,
-                serviceIntro : serviceKey.data.serviceIntro
-
-            };
+				if (serviceKey) {
+	            var serviceObject = {
+	                icon : serviceKey.data.iconFrontPage || "globe",
+	                serviceTitle : serviceKey.data.serviecTitle || "No title added",
+	                serviceIntro : serviceKey.data.serviceIntro || "No description added"
+	            };
+				}
 
             if(serviceObject){
                 services.push(serviceObject);
             }
         }
     }
-    
+
     var model = {
-        title : config.title,
-        intro : config.intro,
+        title : config.title || "No title added",
+        intro : config.intro || "No description added",
         services : services
     };
     var body = libs.thymeleaf.render(view, model);
