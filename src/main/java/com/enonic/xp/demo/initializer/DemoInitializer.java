9308cb67cd22f3f51df1d89e592f1a75cb3f2e4c
Bobby <bellfalasch@gmail.com>
2017-03-30 12:05:31 +0200

    Update demo generator script

--- src/main/java/com/enonic/xp/demo/initializer/DemoInitializer.java
+++ src/main/java/com/enonic/xp/demo/initializer/DemoInitializer.java
@@ -15,20 +15,21 @@ import com.enonic.xp.content.Content;
 import com.enonic.xp.content.ContentConstants;
 import com.enonic.xp.content.ContentPath;
 import com.enonic.xp.content.ContentService;
+import com.enonic.xp.content.CreateContentParams;
 import com.enonic.xp.content.UpdateContentParams;
+import com.enonic.xp.context.Context;
 import com.enonic.xp.context.ContextAccessor;
 import com.enonic.xp.context.ContextBuilder;
-import com.enonic.xp.context.Context;
+import com.enonic.xp.data.PropertyTree;
 import com.enonic.xp.export.ExportService;
 import com.enonic.xp.export.ImportNodesParams;
 import com.enonic.xp.export.NodeImportResult;
 import com.enonic.xp.index.IndexService;
 import com.enonic.xp.node.NodePath;
+import com.enonic.xp.schema.content.ContentTypeName;
 import com.enonic.xp.security.PrincipalKey;
 import com.enonic.xp.security.RoleKeys;
-import com.enonic.xp.security.SecurityService;
 import com.enonic.xp.security.User;
-import com.enonic.xp.security.UserStoreKey;
 import com.enonic.xp.security.acl.AccessControlEntry;
 import com.enonic.xp.security.acl.AccessControlList;
 import com.enonic.xp.security.acl.Permission;
@@ -36,35 +37,28 @@ import com.enonic.xp.security.auth.AuthenticationInfo;
 import com.enonic.xp.vfs.VirtualFile;
 import com.enonic.xp.vfs.VirtualFiles;
 
-
 @Component(immediate = true)
-public class DemoInitializer
+public final class DemoInitializer
 {
 
     private static final AccessControlList PERMISSIONS =
-        AccessControlList.of( AccessControlEntry.create().principal( RoleKeys.EVERYONE ).allow( Permission.READ ).build(),
-                              AccessControlEntry.create().principal( RoleKeys.CONTENT_MANAGER_ADMIN ).allowAll().build(),
-                              AccessControlEntry.create().principal( RoleKeys.CONTENT_MANAGER_APP ).allow( Permission.READ ).build(),
-                              AccessControlEntry.create().principal( RoleKeys.ADMIN ).allowAll().build() );
-
+        AccessControlList.of( AccessControlEntry.create().principal( PrincipalKey.ofAnonymous() ).allow( Permission.READ ).build(),
+                              AccessControlEntry.create().principal( RoleKeys.EVERYONE ).allow( Permission.READ ).build(),
+                              AccessControlEntry.create().principal( RoleKeys.AUTHENTICATED ).allowAll().build(),
+                              AccessControlEntry.create().principal( RoleKeys.CONTENT_MANAGER_ADMIN ).allowAll().build() );
 
     private ContentService contentService;
 
     private ExportService exportService;
 
-    private SecurityService securityService;
-
     private IndexService indexService;
 
     private final Logger LOG = LoggerFactory.getLogger( DemoInitializer.class );
-
-    private static final PrincipalKey SUPER_USER_KEY = PrincipalKey.ofUser( UserStoreKey.system(), "su" );
 
     @Activate
     public void initialize()
         throws Exception
     {
-
         if ( this.indexService.isMaster() )
         {
             runAs( createInitContext(), () -> {
@@ -105,6 +99,8 @@ public class DemoInitializer
 
         logImport( nodeImportResult );
 
+        createLargeTree();
+
         // set permissions
         final Content demoContent = contentService.getByPath( demoSitePath );
         if ( demoContent != null )
@@ -119,7 +115,6 @@ public class DemoInitializer
 
             contentService.applyPermissions( ApplyContentPermissionsParams.create().
                 contentId( demoContent.getId() ).
-                overwriteChildPermissions( true ).
                 build() );
         }
     }
@@ -143,6 +138,15 @@ public class DemoInitializer
         {
             LOG.info( importError.getMessage(), importError.getException() );
         }
+    }
+
+    private CreateContentParams.Builder makeFolder()
+    {
+        return CreateContentParams.create().
+            owner( PrincipalKey.ofAnonymous() ).
+            contentData( new PropertyTree() ).
+            type( ContentTypeName.folder() ).
+            inheritPermissions( true );
     }
 
     private boolean hasContent( final ContentPath path )
@@ -170,15 +174,40 @@ public class DemoInitializer
     }
 
     @Reference
-    public void setSecurityService( final SecurityService securityService )
-    {
-        this.securityService = securityService;
-    }
-
-    @Reference
     public void setIndexService( final IndexService indexService )
     {
         this.indexService = indexService;
+    }
+
+    private void createLargeTree()
+    {
+        final ContentPath largeTreePath = ContentPath.from( "/large-tree" );
+        if ( !hasContent( largeTreePath ) )
+        {
+            contentService.create( makeFolder().
+                name( "large-tree" ).
+                displayName( "Large tree" ).
+                parent( ContentPath.ROOT ).
+                permissions( PERMISSIONS ).
+                inheritPermissions( false ).
+                build() );
+
+            for ( int i = 1; i <= 2; i++ )
+            {
+                Content parent = contentService.create( makeFolder().
+                    displayName( "large-tree-node-" + i ).
+                    displayName( "Large tree node " + i ).
+                    parent( largeTreePath ).build() );
+
+                for ( int j = 1; j <= 100; j++ )
+                {
+                    contentService.create( makeFolder().
+                        displayName( "large-tree-node-" + i + "-" + j ).
+                        displayName( "Large tree node " + i + "-" + j ).
+                        parent( parent.getPath() ).build() );
+                }
+            }
+        }
     }
 
     private <T> T runAs( final Context context, final Callable<T> runnable )
