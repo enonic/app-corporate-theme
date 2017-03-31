afda0704e8806636a8c9833e317480950a26b3ae
Sepideh <ska@enonic.com>
2017-03-31 13:24:27 +0200

    error handling

--- src/main/resources/site/error/error/error.js
+++ src/main/resources/site/error/error/error.js
@@ -1,39 +1,29 @@
 var thymeleaf = require('/lib/xp/thymeleaf');
+var util = require('/lib/enonic/util');
 
-var view= resolve('page-not-found.html');
+var view404= resolve('err404.html');
+var viewGeneric = resolve('error.html');
+
 
 exports.handle404 = function (err) {
-
-    // Handle debug mode and preview mode
-    var debugMode = err.request.params.debug === 'true';
-    if (debugMode && err.request.mode === 'preview') {
-        return;
-    }
-
-    var siteConfig = libs.portal.getSiteConfig();
-    var site = libs.portal.getSite();
-
-    var redirectPageId = siteConfig.errorPage || site._id; 
-    var redirectPageUrl = libs.portal.pageUrl({
-        'id': redirectPageId
-    });
-    
-    return {
-        status: 404,
-        redirect : redirectPageUrl
-    }
-};
-
-exports.handleError = function (err) {
-    var debugMode = err.request.params.debug === 'true';
-    if (debugMode && err.request.mode === 'preview') {
-        return;
-    }
-    
-    var body = "Feil med serveren";
-
+    var body = thymeleaf.render(view404, {});
+    util.log(err);
     return {
         contentType: 'text/html',
         body: body
     }
+};
+
+exports.handleError = function(err){
+
+    var debugMode = err.request.params.debug === 'true';
+    if (debugMode && err.request.mode === 'preview'){
+        return;
+    }
+    
+    var model = {
+        errorCode : err.status
+    };
+    var body = thymeleaf.render(viewGeneric, model);
+    return { body : body}
 };
