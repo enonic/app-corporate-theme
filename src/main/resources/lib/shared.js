26e7612fb5fceae8f8668a883c95ffbba7e9eb69
Bobby <bellfalasch@gmail.com>
2017-03-31 12:58:56 +0200

    Set up shared.js as local lib

---
+++ src/main/resources/lib/shared.js
@@ -0,0 +1,10 @@
+var libs = {
+    portal : require('/lib/xp/portal'),
+    contentLib : require('/lib/xp/content'),
+    util : require('/lib/enonic/util')
+};
+
+// Local library
+exports.getPortfolioData = function(req) {
+	// Do things ...
+};
--- src/main/resources/site/parts/portfolio-frontpage/portfolio-frontpage.js
+++ src/main/resources/site/parts/portfolio-frontpage/portfolio-frontpage.js
@@ -2,7 +2,8 @@ var libs = {
     portal : require('/lib/xp/portal'),
     thymeleaf : require('/lib/xp/thymeleaf'),
     contentLib : require('/lib/xp/content'),
-    util : require('/lib/enonic/util')
+    util : require('/lib/enonic/util'),
+    shared : require('/lib/shared')
 };
 
 var view = resolve('portfolio-frontpage.html');
