251dc317704282a0c1f7db83e9ecc5af22d1e553
Sepideh <ska@enonic.com>
2017-04-03 09:16:11 +0200

    error handling updated

--- src/main/resources/site/error/error.js
+++ src/main/resources/site/error/error.js
@@ -1,25 +1,24 @@
 var thymeleaf = require('/lib/xp/thymeleaf');
-var util = require('/lib/enonic/util');
 
 var viewError = resolve('error.html');
 var view404= resolve('err404.html');
 
 exports.handle404 = function (err) {
-    var body = thymeleaf.render(view404, {});
 
-    util.log(err);
-    
-    return {
-        contentType: 'text/html',
-        body: body
-    }
+    var body = thymeleaf.render(view404, {});
+    return { body: body}
 };
 
 exports.handleError = function(err){
+
+    var debugMode = err.request.params.debug === 'true';
+    if (err.request.mode === 'preview') {
+        return;
+    }
     var model = {
         errStatus :  err.status,
         errMsg : err.message
-    }
+    };
     var body = thymeleaf.render(viewError, model);
     return {body : body}
 };
