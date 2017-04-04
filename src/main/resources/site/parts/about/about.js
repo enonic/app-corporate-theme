0f8ce3a1a994589d260b63934d7fa17baa2d13a0
Sepideh <ska@enonic.com>
2017-04-04 08:40:00 +0200

    merge conflict fixes

--- src/main/resources/site/parts/about/about.js
+++ src/main/resources/site/parts/about/about.js
@@ -9,7 +9,8 @@ var view = resolve('about.html');
 
 exports.get = function(){
 
-    var config = libs.portal.getComponent().config;
+    var component = libs.portal.getComponent();
+    var config = component.config;
     var employees = [];
     var facebookUrl = null;
     var twitterUrl = null;
@@ -17,7 +18,7 @@ exports.get = function(){
     var googleUrl = null;
 
     var employeesArray = config.team ? libs.util.data.forceArray(config.team) : null;
-    if(employeesArray){
+    if (employeesArray) {
         for(var i=0 ; i < employeesArray.length; i++) {
             var employeeKey = libs.content.get({
                 key: employeesArray[i]
@@ -27,37 +28,40 @@ exports.get = function(){
 			var selected = employeeKey.data.socialLinks._selected;
 				var socialLinks = employeeKey.data.socialLinks;
 
-			if (selected.indexOf("twitter") >= 0) {
-				twitterUrl = socialLinks.twitter.twitterUrl;
-			}
+					// Check for use of social links
+	            if (selected.indexOf("twitter") >= 0) {
+	                twitterUrl = socialLinks.twitter.twitterUrl;
+	            }
+	            if (selected.indexOf("facebook") >= 0) {
+	                facebookUrl = socialLinks.facebook.facebookUrl;
+	            }
+	            if (selected.indexOf("linkedin") >= 0) {
+	                linkedinUrl = socialLinks.linkedin.linkedinUrl;
+	            }
+	            if (selected.indexOf("google") >= 0) {
+	                googleUrl = socialLinks.google.googleUrl;
+	            }
 
-			if (selected.indexOf("facebook") >= 0) {
-				facebookUrl = socialLinks.facebook.facebookUrl;
-			}
+					var imageUrl = null;
+					if (employeeKey.data.photo) {
+						imageUrl = libs.portal.imageUrl({
+							 id: employeeKey.data.photo,
+							 scale: 'block(250,167)',
+						});
+					}
 
-			if (selected.indexOf("linkedin") >= 0) {
-				linkedinUrl = socialLinks.linkedin.linkedinUrl;
-			}
+	            var employeeObject = {
+	                photo: imageUrl,
+	                name: employeeKey.displayName,
+	                intro: employeeKey.data.intro,
+	                facebookUrl: facebookUrl,
+	                twitterUrl: twitterUrl,
+	                linkedinUrl: linkedinUrl,
+	                googleUrl: googleUrl
+	            };
 
-			if (selected.indexOf("google") >= 0) {
-				googleUrl = socialLinks.google.googleUrl;
-			}
-
-			var employeeObject = {
-				photo: libs.portal.imageUrl({
-					id: employeeKey.data.photo,
-					scale: 'block(250,167)',
-				}),
-				name: employeeKey.displayName,
-				intro: employeeKey.data.intro,
-				facebookUrl: facebookUrl,
-				twitterUrl: twitterUrl,
-				linkedinUrl: linkedinUrl,
-				googleUrl: googleUrl
-			};
-
-			employees.push(employeeObject);
-			}
+	            employees.push(employeeObject);
+				}
         }
     }
 
@@ -71,5 +75,5 @@ exports.get = function(){
     };
 
     var body = libs.thymeleaf.render(view, model);
-    return {body : body};
+    return { body : body };
 };
