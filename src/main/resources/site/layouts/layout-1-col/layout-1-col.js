9cd8151ec3ee262859de1157a0cefd8dd872026a
Bellfalasch <bellfalasch@gmail.com>
2017-07-07 13:11:35 +0200

    Indent and multiple other fixes

--- src/main/resources/site/layouts/layout-1-col/layout-1-col.js
+++ src/main/resources/site/layouts/layout-1-col/layout-1-col.js
@@ -7,9 +7,10 @@ var view = resolve('layout-1-col.html');
 
 exports.get = function(req) {
     var component = libs.portal.getComponent();
+	var classList = component.config.fullWidth ? "container-fluid" : "container";
     var model = {
         mainRegion: component.regions['main'],
-        container: component.config.fullWidth ? "container-fluid" : "container",
+        container: 'layout-col-1 ' + classList,
         backgroundColor: component.config.backgroundColor || null
     };
     var body = libs.thymeleaf.render(view, model);
