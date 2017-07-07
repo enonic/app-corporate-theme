82075e6f7a7b61bc53bb24d41290d3543238b46f
Bellfalasch <bellfalasch@gmail.com>
2017-07-07 10:54:20 +0200

    Fix all the broken things with layout col 1
    Background color setting not used, and more

--- src/main/resources/site/layouts/layout-1-col/layout-1-col.js
+++ src/main/resources/site/layouts/layout-1-col/layout-1-col.js
@@ -3,24 +3,15 @@ var libs = {
     thymeleaf: require('/lib/xp/thymeleaf'),
     util : require('/lib/enonic/util')
 };
-
-// SETTINGS
 var view = resolve('layout-1-col.html');
 
-// Handle the GET request
 exports.get = function(req) {
-
-    var component = libs.portal.getComponent();
-    var config = component.config;
-    var fullWidth = config.fullWidth ? "container-fluid" : "container";
-    var backgroundColor = config.backgroundColor || "#252525";
+    var component = libs.portal.getComponent().config;
     var model = {
         mainRegion: component.regions['main'],
-        container : fullWidth,
-        backgroundColor : backgroundColor
-        
+        container: config.fullWidth ? "container-fluid" : "container",
+        backgroundColor: config.backgroundColor || "#252525"
     };
-    // Rendering
     var body = libs.thymeleaf.render(view, model);
     return { body: body };
 };
