06cf6d8c311f3d69f55d1ed734bc142b4c850fea
bellfalasch <bellfalasch@gmail.com>
2018-09-06 14:22:34 +0200

    Use new format for bg-color on all layouts

--- src/main/resources/site/layouts/layout-1-col/layout-1-col.js
+++ src/main/resources/site/layouts/layout-1-col/layout-1-col.js
@@ -7,11 +7,15 @@ var view = resolve('layout-1-col.html');
 
 exports.get = function(req) {
     var component = libs.portal.getComponent();
-	var classList = component.config.fullWidth ? "container-fluid" : "container";
+	 var config = component.config;
+
+ 	 var containerClass = config.fullWidthBackground.yes ? "container-fluid" : "container";
+ 	 var containerColor = config.fullWidthBackground.yes ? config.fullWidthBackground.yes.backgroundColor : 'transparent';
+
     var model = {
         mainRegion: component.regions['main'],
-        container: 'layout-col-1 ' + classList,
-        backgroundColor: component.config.backgroundColor || null
+        containerClass: containerClass,
+		  containerColor: containerColor
     };
     var body = libs.thymeleaf.render(view, model);
     return { body: body };
